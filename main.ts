324dbb7065a496a7c15f4dad93cb9717bc1c403a
Thang Nguyen <quaywin@gmail.com>
2023-11-18 17:06:23 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,24 @@
-basic.forever(function () {
-	
-})
+namespace BoardShield {
+    export function turn_on_fan(pin: AnalogPin) {
+        pins.analogWritePin(pin, 500)
+    }
+
+    //% block
+    //% group="Fan"
+    export function turn_on_max_fan(pin: AnalogPin) {
+        pins.analogWritePin(pin, 1023)
+    }
+
+    //% block
+    //% group="Fan"
+    export function turn_off_fan(pin: AnalogPin) {
+        pins.analogWritePin(pin, 0)
+    }
+
+    //% block
+    //% speed.min=0 speed.max=1023
+    //% group="Fan"
+    export function set_fan_speed(pin: AnalogPin, speed: number) {
+        pins.analogWritePin(pin, speed)
+    }
+}
