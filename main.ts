998202f0210e9e398f0375bed2fe846b94aeec1c
Thang Nguyen <quaywin@gmail.com>
2024-09-05 16:50:53 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -20,9 +20,11 @@ namespace SK_Modules {
         RightDown = 6
     }
 
+    let initialized = false
     const PCA9685_ADD = 0x40
     const LED0_ON_L = 0x06
-
+    const MODE1 = 0x00
+    const PRESCALE = 0xFE
     export enum TopButtons {
         L2 = 3, //21 0 - 255, 20 0-3
         R2 = 3, // 24 0 - 3, 25 0 -255
@@ -92,10 +94,47 @@ namespace SK_Modules {
     const listEvents: Event[] = []
     const listActionButtonEvents: ActionButtonEvent[] = []
 
+    function i2cwrite(addr: number, reg: number, value: number) {
+        let buf = pins.createBuffer(2)
+        buf[0] = reg
+        buf[1] = value
+        pins.i2cWriteBuffer(addr, buf)
+    }
+
+    function i2cread(addr: number, reg: number) {
+        pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE);
+        let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
+        return val;
+    }
+
+    function setFreq(freq: number): void {
+        // Constrain the frequency
+        let prescaleval = 25000000;
+        prescaleval /= 4096;
+        prescaleval /= freq;
+        prescaleval -= 1;
+        let prescale = prescaleval; //Math.Floor(prescaleval + 0.5);
+        let oldmode = i2cread(PCA9685_ADD, MODE1);
+        let newmode = (oldmode & 0x7F) | 0x10; // sleep
+        i2cwrite(PCA9685_ADD, MODE1, newmode); // go to sleep
+        i2cwrite(PCA9685_ADD, PRESCALE, prescale); // set the prescaler
+        i2cwrite(PCA9685_ADD, MODE1, oldmode);
+        control.waitMicros(5000);
+        i2cwrite(PCA9685_ADD, MODE1, oldmode | 0xa1);
+    }
+
+    function initPCA9685(): void {
+        i2cwrite(PCA9685_ADD, MODE1, 0x00)
+        setFreq(50);
+        initialized = true
+    }
+
     function setPwm(channel: number, on: number, off: number): void {
         if (channel < 0 || channel > 15)
             return;
-
+        if (!initialized) {
+            initPCA9685();
+        }
         let buf = pins.createBuffer(5);
         buf[0] = LED0_ON_L + 4 * channel;
         buf[1] = on & 0xff;
@@ -208,7 +247,7 @@ namespace SK_Modules {
             //     const actionButtonEvent = listActionButtonEvents[i]
             //     checkButton(actionButtonEvent.button, buttonAction, actionButtonEvent.callback)
             // }
-            // basic.pause(10)
+            basic.pause(10)
         })
     }
 
@@ -219,6 +258,8 @@ namespace SK_Modules {
     //% enumMemberName="button"
     //% enumInitialMembers="Up, Down, Left, Right"
     export function GamePadDirectionButton(button: Buttons): boolean {
+        console.log(button)
+        console.log(buttonDirection)
         return buttonDirection == button;
     }
 
